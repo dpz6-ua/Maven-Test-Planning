faf108dde31a36a3d77ae45e53c3b28bb443a5b2
David Piotr Znojek <dawidznojek@gmail.com>
2025-04-13 20:05:52 +0200

    eh2

---
+++ P07B-Integracion/dbunit/matriculacion/matriculacion-proxy/src/test/java/AlumnoDAOTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AlumnoDAOTest {
+    @Test
+    public void A1(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void A2(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void A3(){
+        assertTrue(true);
+    }
+}
